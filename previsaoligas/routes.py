66fabeb4aa5874d8e0892a5ad839005d9f721be7
alanhassan <76003761+alanhassan@users.noreply.github.com>
2023-08-14 23:20:47 -0300

    Update routes.py

--- previsaoligas/routes.py
+++ previsaoligas/routes.py
@@ -77,7 +77,7 @@ def login():
         else:
             flash(f'Falha no Login. E-mail ou Senha Incorretos', 'alert-danger')
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
-        senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data).decode("utf-8")
         usuario = Usuario(username=form_criarconta.username.data, email=form_criarconta.email.data
                           , senha=senha_cript)
         database.session.add(usuario)
