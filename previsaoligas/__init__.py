e21b89be2869b46c240320b4fdcf64f1e3ef9d04
Alan Werner Hassan <alan.hassan@shopee.com>
2023-08-14 23:37:27 -0300

    atualizando

--- previsaoligas/__init__.py
+++ previsaoligas/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
 
 app = Flask(__name__)
 
@@ -18,6 +19,18 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
+
+from previsaoligas import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("Base de Dados criada")
+else:
+    print("Base de Dados já existente")
+
 
 from previsaoligas import routes
 
